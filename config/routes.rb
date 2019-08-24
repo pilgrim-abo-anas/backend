783bd213557c6157e9da833655776ab417722a85
Salman778 <alk-333@hotmail.com>
2019-08-25 02:42:48 +0300

    root to index of pligrams

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root "pilgrims#index"
   resources :pilgrims
-  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :admins, controllers: { registrations: 'admins/registrations',
                                      sessions: 'admins/sessions', unlocks: 'admins/unlocks',
