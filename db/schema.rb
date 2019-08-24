58b9134f074edcecc7f9d83d78e537eacf7e8389
Salman778 <alk-333@hotmail.com>
2019-08-25 02:16:18 +0300

    write Pilgrim into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_222453) do
+ActiveRecord::Schema.define(version: 2019_08_24_231434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,4 +31,19 @@ ActiveRecord::Schema.define(version: 2019_08_24_222453) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "pilgrims", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "type"
+    t.string "price"
+    t.string "sheep"
+    t.string "blood"
+    t.boolean "status"
+    t.bigint "admin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_pilgrims_on_admin_id"
+  end
+
+  add_foreign_key "pilgrims", "admins"
 end
