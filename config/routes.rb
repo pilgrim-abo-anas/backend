d14acd86f7909b4ad21c4a0b437d4a1a453956cc
Salman778 <alk-333@hotmail.com>
2019-08-25 01:52:30 +0300

    paths for controoler for admin and home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: { registrations: 'admins/registrations',
+                                     sessions: 'admins/sessions', unlocks: 'admins/unlocks',
+                                     passwords: 'admins/passwords', confirmations: 'admins/confirmations'}
 end
