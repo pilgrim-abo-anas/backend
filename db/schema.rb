d92fecca533b63c4b2e288fb5142726d15b92342
Salman778 <alk-333@hotmail.com>
2019-08-25 02:52:00 +0300

    add pilgrim_type coulmn to database in pilgrims

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_234355) do
+ActiveRecord::Schema.define(version: 2019_08_24_234854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_08_24_234355) do
     t.bigint "admin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pilgrim_type"
     t.index ["admin_id"], name: "index_pilgrims_on_admin_id"
   end
 
