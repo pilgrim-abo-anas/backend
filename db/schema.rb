84316b135bb1c1a95eaaa19b3b0419c111033392
Salman778 <alk-333@hotmail.com>
2019-08-25 02:46:11 +0300

    Remove type coulmn from database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_231434) do
+ActiveRecord::Schema.define(version: 2019_08_24_234355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2019_08_24_231434) do
   create_table "pilgrims", force: :cascade do |t|
     t.string "name"
     t.string "phone"
-    t.string "type"
     t.string "price"
     t.string "sheep"
     t.string "blood"
