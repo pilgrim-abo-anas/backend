6d18d99978198dffc87b2d735ca6208da906f008
Salman778 <alk-333@hotmail.com>
2019-08-25 03:26:23 +0300

    delte the type and put price because in the past i delete the price by mistake when i want to delete type

--- app/controllers/pilgrims_controller.rb
+++ app/controllers/pilgrims_controller.rb
@@ -69,6 +69,6 @@ class PilgrimsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pilgrim_params
-      params.require(:pilgrim).permit(:name, :phone, :type, :pilgrim_type, :sheep, :blood, :status)
+      params.require(:pilgrim).permit(:name, :phone, :price, :pilgrim_type, :sheep, :blood, :status)
     end
 end
