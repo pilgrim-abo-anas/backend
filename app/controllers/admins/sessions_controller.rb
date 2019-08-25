3517eda66756d20078d63f26dfc1b634d85dd648
Salman778 <alk-333@hotmail.com>
2019-08-25 03:15:21 +0300

    redirect to pilgrims page after singin signup update

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -18,8 +18,12 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
+  def after_sign_in_path_for(resource)
+    super(resource)
+    '/pilgrims'
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
